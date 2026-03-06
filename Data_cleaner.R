ac92e3e57cf0e7ed47db4628230650c76b0810e4
riccardoopiva <265052276+riccardoopiva@users.noreply.github.com>
2026-03-06 22:58:31 +0000

    cleaned col realted to dates scaled from 1:10 to 100pt distrubuted for waves 6 to 9

--- Data_cleaner.R
+++ Data_cleaner.R
@@ -2,42 +2,27 @@
 library(tidyverse)
 library(janitor)
 
-data <- read_csv("Speed_Dating.csv")
-speed_dating <- data %>%
-  clean_names()
-
-#-----------------------------------------------
-# Data cleaning \anomalies correction
-## id
-speed_dating[8378,'id'] = 22 # NaN -> 22
-
-## pid
-x <-which(is.na(speed_dating$pid))
-dates[x,c('iid','id','gender','wave','partner','pid')]
-y<-which (dates$id == 7 & dates$wave == 5)
-dates[y,c('iid','id','gender','wave','partner','pid')]
-setdiff(min(dates$iid):max(dates$iid), dates$iid) #iid 118 did not participate
-# drop rows related to 118
+speed_dating <- read_csv("Speed_Dating.csv")
 speed_dating <- speed_dating %>%
-  filter(!is.na(pid))
-
+  clean_names() %>%
+  mutate(index = row_number(), .before = 1) # add index column
 
 #-----------------------------------------------
 # Dates informations
 ## datset with columns related to every single date
 dates <- speed_dating %>%
-  select(iid:met_o)
+  select(index:met_o)
 
 ## scorecard filled after each date + iid,id,partner
 scorecard <- speed_dating %>%
-  select(c(iid,id,partner,dec:match_es))
+  select(c(iid,id,pid,partner,dec:match_es))
 
 #-----------------------------------------------
 # datasets with information unique for every iid
 
 ## data filled to sign up + iid //Time1
 signup <- speed_dating %>%
-  select(c(iid, age:amb5_1)) %>% 
+  select(c(iid, wave, age:amb5_1)) %>% 
   distinct() #-- collapsed, 551 distinct iid
 
 ## data filled half way thru + iid
@@ -54,6 +39,100 @@ followup1 <- speed_dating %>%
 followup2 <- speed_dating %>%
   select(c(iid,you_call:amb5_3)) %>%
   distinct()
+
+
+#-----------------------------------------------
+# Data cleaning \anomalies correction
+## id
+speed_dating[8378,'id'] = 22 # NaN -> 22
+
+## pid
+x <-which(is.na(speed_dating$pid))
+speed_dating[x,c('index','iid','id','gender','wave','partner','pid')]
+y<-which (dates$id == 7 & dates$wave == 5)
+dates[y,c('index','iid','id','gender','wave','partner','pid')]
+setdiff(min(dates$iid):max(dates$iid), dates$iid) #iid 118 did not participate
+# drop rows related to 118
+dates <- speed_dating %>%
+  filter(!is.na(pid))
+
+### Dates
+## age_o to pf_o_sha
+# get the attribute of the partner from their signup form
+for (i in 1:nrow(dates)) {
+  if (is.na(dates$age_o[i])) { 
+    dates$age_o[i] <- signup$age[dates$pid[i]]
+  } # NAs 104 -> 40
+  if (is.na(dates$race_o[i])) { 
+    dates$race_o[i] <- signup$race[dates$pid[i]]
+  } # NAs 73 -> 40
+  if (is.na(dates$pf_o_att[i])) { 
+    dates$pf_o_att[i] <- signup$attr1_1[dates$pid[i]]
+  } # NAs 89 -> 56
+  if (is.na(dates$pf_o_sin[i])) { 
+    dates$pf_o_sin[i] <- signup$sinc1_1[dates$pid[i]]
+  } # NAs 89 -> 56
+  if (is.na(dates$pf_o_int[i])) { 
+    dates$pf_o_int[i] <- signup$intel1_1[dates$pid[i]]
+  } # NAs 89 -> 56
+  if (is.na(dates$pf_o_fun[i])) { 
+    dates$pf_o_fun[i] <- signup$fun1_1[dates$pid[i]]
+  } # NAs 98 -> 56
+  if (is.na(dates$pf_o_amb[i])) { 
+    dates$pf_o_amb[i] <- signup$amb1_1[dates$pid[i]]
+  } # NAs 107 -> 65
+  if (is.na(dates$pf_o_sha[i])) { 
+    dates$pf_o_sha[i] <- signup$shar1_1[dates$pid[i]]
+  } # NAs 129 -> 65
+}
+
+# attr_o to met_o / the data are missing
+dates <- dates %>% #chat GPT
+  left_join(
+    scorecard %>%
+      select(iid, pid, attr, sinc, intel, fun, amb, shar, like),
+    by = c("pid" = "iid", "iid" = "pid")
+  ) %>%
+  mutate(
+    attr_o  = coalesce(attr_o,  attr),
+    sinc_o  = coalesce(sinc_o,  sinc),
+    intel_o = coalesce(intel_o, intel),
+    fun_o   = coalesce(fun_o,   fun),
+    amb_o   = coalesce(amb_o,   amb),
+    shar_o  = coalesce(shar_o,  shar),
+    like_o  = coalesce(like_o,  like)
+  ) %>%
+  select(-attr, -sinc, -intel, -fun, -amb, -shar, -like)
+
+### Scorecard
+# scaling 1-10 to 100pt for wave 6 to 9
+for (i in 1:nrow(signup)) {
+  if (signup$wave[i] %in% 6:9) {
+    tot1 <- sum(signup[i,39:44])
+    signup$attr1_1[i] = signup$attr1_1[i]/tot1 *100
+    signup$sinc1_1[i] = signup$sinc1_1[i]/tot1 *100
+    signup$intel1_1[i] = signup$intel1_1[i]/tot1 *100
+    signup$fun1_1[i] = signup$fun1_1[i]/tot1 *100
+    signup$amb1_1[i] = signup$amb1_1[i]/tot1 *100
+    signup$shar1_1[i] = signup$shar1_1[i]/tot1 *100
+    
+    tot2 <- sum(signup[i,51:56])
+    signup$attr2_1[i] = signup$attr2_1[i]/tot2 *100
+    signup$sinc2_1[i] = signup$sinc2_1[i]/tot2 *100
+    signup$intel2_1[i] = signup$intel2_1[i]/tot2 *100
+    signup$fun2_1[i] = signup$fun2_1[i]/tot2 *100
+    signup$amb2_1[i] = signup$amb2_1[i]/tot2 *100
+    signup$shar2_1[i] = signup$shar2_1[i]/tot2 *100
+    
+    tot4 <- sum(signup[i,45:50])
+    signup$attr4_1[i] = signup$attr4_1[i]/tot4 *100
+    signup$sinc4_1[i] = signup$sinc4_1[i]/tot4 *100
+    signup$intel4_1[i] = signup$intel4_1[i]/tot4 *100
+    signup$fun4_1[i] = signup$fun4_1[i]/tot4 *100
+    signup$amb4_1[i] = signup$amb4_1[i]/tot4 *100
+    signup$shar4_1[i] = signup$shar4_1[i]/tot4 *100
+  }
+}
 
 ## to count NA in every column of a dataset
 count_na <- function(x){
